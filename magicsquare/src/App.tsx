65d1c424b72e8e75f9a91839901aa779971a3a83
matthcol <matthias.colin@gmail.com>
2023-10-03 16:29:26 +0200

    play with repetition possible

--- magicsquare/src/App.tsx
+++ magicsquare/src/App.tsx
@@ -1,19 +1,54 @@
 import { useState } from "react";
 
-function Square({ value }: { value: number }) {
-  const valueDisplayed = value === 0 ? " " : `${value}`;
-  return <div className="square">{valueDisplayed}</div>;
+function makeArray(n: number): number[] {
+  return [...Array(n).keys()].map((i) => i + 1);
 }
 
-function Board({ squares }: { squares: number[][] }) {
+function makeArray2D(n: number): number[][] {
+  return Array(n)
+    .fill(0)
+    .map(() => new Array(n).fill(0));
+}
+
+function Square({
+  value,
+  actionSquare
+}: {
+  value: number;
+  actionSquare?: () => void;
+}) {
+  // console.log("Square: value = ", value);
+  const valueDisplayed = value === 0 ? " " : `${value}`;
+  // console.log("Square: value displayed= ", valueDisplayed);
+  return (
+    <div className="square" onClick={actionSquare}>
+      {valueDisplayed}
+    </div>
+  );
+}
+
+function Board({
+  squares,
+  actionSquare
+}: {
+  squares: number[][];
+  actionSquare?: (i: number, j: number) => void;
+}) {
+  console.log("(re)display board:", squares);
   return (
     <div>
       {squares.map((row, i) => (
-        <div className="board-row">
+        <div key={i} className="board-row">
           {row.map((n, j) => {
             const myKey = `${i}_${j}`;
-            console.log(myKey);
-            return <Square key={myKey} value={0} />;
+            // console.log(myKey);
+            return (
+              <Square
+                key={myKey}
+                value={n}
+                actionSquare={() => actionSquare(i, j)}
+              />
+            );
           })}
         </div>
       ))}
@@ -21,11 +56,23 @@ function Board({ squares }: { squares: number[][] }) {
   );
 }
 
-function Numbers({ numbers }: { numbers: number[] }) {
+function Numbers({
+  numbers,
+  numberSelected,
+  action
+}: {
+  numbers: number[];
+  numberSelected: number;
+  action: (n: number) => void;
+}) {
   return (
     <div className="nbStack">
       {numbers.map((n) => (
-        <div key={n} className="nb">
+        <div
+          key={n}
+          className={n === numberSelected ? "nb-selected" : "nb"}
+          onClick={() => action(n)}
+        >
           {n}
         </div>
       ))}
@@ -34,21 +81,57 @@ function Numbers({ numbers }: { numbers: number[] }) {
 }
 
 export default function MyApp() {
-  const n = 3;
+  const [size, setSize] = useState(3);
   // numbers to place on the board
-  const [numbers, setNumbers] = useState(
-    [...Array(n * n).keys()].map((i) => i + 1)
-  );
+  const [numbers, setNumbers] = useState(makeArray(size * size));
   // numbers placed on the board (0 if empty)
-  const [squares, setSquares] = useState(
-    Array(n)
-      .fill(0)
-      .map(() => new Array(n).fill(0))
-  );
+  const [squares, setSquares] = useState(makeArray2D(size));
+
+  const [numberSelected, setNumberSelected] = useState(0);
+
+  function resetGame(event) {
+    const newSize = parseInt(event.target.value, 10);
+    if (newSize <= 0) return;
+    setSize(newSize);
+    setNumberSelected(0);
+    setNumbers(makeArray(newSize * newSize));
+    setSquares(makeArray2D(newSize));
+  }
+
+  function selectNumber(n: number) {
+    // console.log("Number selected:", n);
+    setNumberSelected(n);
+  }
+
+  // function squareClic(i: number, j: number) {
+  //   // fake action here as a prototype
+  //   console.log("Clic square:", i, j);
+  //   const nextSquares = squares.slice();
+  //   ++nextSquares[i][j];
+  //   console.log("Next square:", nextSquares);
+  //   setSquares(nextSquares);
+  // }
+
+  function squareClic(i: number, j: number) {
+    // fake action here as a prototype
+    console.log("Clic square:", i, j);
+    const nextSquares = squares.slice();
+    nextSquares[i][j] = numberSelected;
+    console.log("Next square:", nextSquares);
+    setSquares(nextSquares);
+  }
+
   return (
-    <div className="game">
-      <Board squares={squares} />
-      <Numbers numbers={numbers} />
-    </div>
+    <>
+      <input type="number" value={size} onChange={resetGame} />
+      <div className="game">
+        <Board squares={squares} actionSquare={squareClic} />
+        <Numbers
+          numbers={numbers}
+          numberSelected={numberSelected}
+          action={selectNumber}
+        />
+      </div>
+    </>
   );
 }
